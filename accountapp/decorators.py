0e844d1ac985efc1bb58d71eb863e2372627ea35
kangt <kangtor918@gmail.com>
2021-07-21 10:43:53 +0900

    2021.07.21 1ban class commit 3

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -1,54 +1,18 @@
-def decorator(func):
-    def decorated(input_text):
-        print('start')
-        func(input_text)
-        print('end')
-    return decorated
+from django.http import HttpResponseForbidden
+from django.contrib.auth.models import User
 
-def decorator2(func):
-    def decorated(w, h, user):
-        if not (w > 0 and h > 0):
-            raise ValueError('Input must be positive value')
-        return func(w,h,user)
 
-    return decorated
-
-def decorator3(func):
-    def decorated(w, h, user):
-        if user.is_authenticated:
-            return func(w,h,user)
+def account_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        target_user = User.objects.get(pk=kwargs['pk'])
+        # User라는 오브젝트를 데이터베이스에서 꺼내옴
+        if target_user == request.user:
+            return func(request, *args, **kwargs)
         else:
-            raise PermissionError('loing required')
+            return HttpResponseForbidden()
 
     return decorated
 
-class User:
-    def __init__(self,a):
-        self.is_authenticated = a
-
-
-
-@decorator
-def hello_world(input_text):
-    print(input_text)
-
-@decorator2
-@decorator3
-def tri_area(w,h, user):
-    return w*h/2
-
-@decorator2
-@decorator3
-def sq_area(w,h, user):
-    return w*h
-
-user1 = User(False)
-
-hello_world('hello world!')
-
-s1=sq_area(2 ,4 , user1)
-
-print(s1)
 
 
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
@@ -49,9 +50,10 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+has_ownership = [login_required, account_ownership_required]
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 # 이 두개로는 로그인의 유무만 확인가능하므로 user와 targetuser가 동일한것도확인해줘야함
 class AccountUpdateView(UpdateView):
     model = User
@@ -61,8 +63,8 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
