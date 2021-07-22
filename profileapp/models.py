a6ffd4f2be2d4e4c6a260fd77799651c5425efc5
kangt <kangtor918@gmail.com>
2021-07-22 15:52:50 +0900

    2021.07.22 1ban class commit 3

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    # user에 1:1로 연결(연결시켜줄클래스, 연결되어있는User객체가사라지면어떻게될지 정책
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+    message = models.CharField(max_length=200, null=True)
