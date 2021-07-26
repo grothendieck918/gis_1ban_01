6204b2117db7d0f7c5fe75cfa8b4b7fbc34df53e
kangt <kangtor918@gmail.com>
2021-07-26 16:21:00 +0900

    2021.07.26 1ban class commit 4

--- gis_01/urls.py
+++ gis_01/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls), # python manage.py createsuperuser 관리자계정 생성
     path('accounts/', include('accountapp.urls')), # accounts/ 이주소로 접근하면 ~
     path('profiles/', include('profileapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
