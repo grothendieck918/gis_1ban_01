3d2688c136b728523e94ef03bace2d2263960712
kangt <kangtor918@gmail.com>
2021-08-09 14:56:56 +0900

    2021.08.9 1ban class commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -5,10 +5,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
+from commentapp.forms import CommentCreationForm
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -25,8 +28,9 @@ class ArticleCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormMixin):
     model = Article
+    form_class = CommentCreationForm
     # html에서 어떤객체를통해 오브젝트를 이동할건지
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
