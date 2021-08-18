731ddc2785b35404db93b6f8a5b1502802696d0a
kangt <kangtor918@gmail.com>
2021-08-18 10:31:17 +0900

    2021.08.18 1ban class commit 5

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -22,10 +24,18 @@ class ProjectCreateView(CreateView):
     # self.object 크리에이트뷰에서만들어진 객체
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
+    # 다수의 객체를 사용할수있게하는 멀티플오브젝트믹스인
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(project=self.object)
+        # 해당프로젝트의 게시글만 필터링
+        return super().get_context_data(object_list=article_list ,**kwargs)
 
 
 class ProjectListView(ListView):
