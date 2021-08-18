0584fa921329d6ce0f30e88e984029ae45e7d18b
kangt <kangtor918@gmail.com>
2021-08-18 10:12:02 +0900

    2021.08.18 1ban class commit 4

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,13 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     # 이 유저 객체가 없어졌을때 남아있는 article객체는 어떻게할것인가 사라지지는않지만 작성지가안보임
-    
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='article', null=True)
+
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     # article폴더가생겨서 그안에 게시글관련 사진이저장됨
