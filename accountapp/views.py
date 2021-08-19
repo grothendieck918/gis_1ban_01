22604f1f2b97e57fc0f224dab2c184484f6b6aba
kangt <kangtor918@gmail.com>
2021-08-19 14:43:29 +0900

    2021.08.19 1ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
@@ -16,6 +17,9 @@ from accountapp.models import HelloWorld
 
 
 # @login_required(login_url = reverse_lazy('accountapp:login')) #기본적으로 accountapp의 앱이름에 login 으로 이동되게 적혀있어서 생략가능
+from articleapp.models import Article
+
+
 @login_required
 def hello_world(request):
 
@@ -49,10 +53,17 @@ class AccountCreateView(CreateView):
         return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
     # 아까 object는 target profile이어엇지만 지금은 targetuser라 바로 pk
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list, **kwargs)
+
 
 has_ownership = [login_required, account_ownership_required]
 
